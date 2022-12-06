f5506b95df7127f4cbc58bf4c357fbb36af5e34a
Robert Hyde <robert.hyde@citrix.com>
2022-12-06 08:03:43 +0000

    Day 6: First draft of both solutions for Day 6, tweaking it slightly to handle situations where we don't know the length of the signal

---
+++ Day6/Program.cs
@@ -0,0 +1,10 @@
+﻿using Common;
+using Day6;
+
+var signalsFilePath = "signals.txt";
+await foreach(var line in FileLoader.GetLinesAsync(signalsFilePath))
+{
+    Console.WriteLine($"Signal: {line}");
+    Console.WriteLine($"Start of Packet Marker: {SignalAnalyzer.FindStartOfPacketMarker(line)}");
+    Console.WriteLine($"Start of Message Marker: {SignalAnalyzer.FindStartOfMessageMarker(line)}");
+}
---
+++ Day6/SignalAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class SignalAnalyzer
+    {
+        public static int FindStartOfPacketMarker(string signal)
+        {
+            return FindUniqueCharacterBlock(signal, 4);
+        }
+
+        internal static object FindStartOfMessageMarker(string signal)
+        {
+            return FindUniqueCharacterBlock(signal, 14);
+        }
+
+        private static int FindUniqueCharacterBlock(string signal, int length)
+        {
+            var markerCounter = new Dictionary<char, int>();
+
+            for (var i = 0; i < signal.Length; i++)
+            {
+                var input = signal[i];
+
+                if (!markerCounter.ContainsKey(input))
+                {
+                    markerCounter[input] = 0;
+                }
+
+                markerCounter[input]++;
+
+                if (i > length - 1)
+                {
+                    markerCounter[signal[i - length]]--;
+                }
+
+                if (MarkerPresent(markerCounter, length))
+                {
+                    return i + 1;
+                }
+
+            }
+
+            return -1;
+        }
+
+        private static bool MarkerPresent(Dictionary<char, int> markerCounter, int markerLength)
+        {
+            var uniqueCount = markerCounter.Where(m => m.Value == 1);
+            return uniqueCount.Count() == markerLength;
+        }
+    }
+}
