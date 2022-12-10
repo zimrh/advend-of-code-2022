8abff99efad855a2290211ed2ffa2551f63ba4bb
Robert Hyde <robert.hyde@citrix.com>
2022-12-10 11:02:31 +0000

    Day 10: Bit hacky but it works

---
+++ Day10/Analyzer.cs
@@ -0,0 +1,74 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10
+{
+    internal class Analyzer
+    {
+        private Dictionary<char, double> Registers = new Dictionary<char, double>();
+        private double Cycle = 1;
+        private double SignalStrength = 0;
+
+        public Analyzer()
+        {
+            Registers.Add('x', 1);
+        }
+
+        public async Task<double> GetSignalStrenghtSum(string programPath)
+        {
+            await foreach (var line in FileLoader.GetLinesAsync(programPath))
+            {
+                ParseAndExecuteLine(line);
+            }
+
+            return SignalStrength;
+        }
+
+        private void ParseAndExecuteLine(string line)
+        {
+            if (string.IsNullOrWhiteSpace(line)) { return; }
+
+            var split = line.Split(' ');
+            var command = split[0];
+            IncrementCycle();
+
+            if (command == "addx")
+            {
+                CheckCyclesAndAddIfNeeded();
+
+                IncrementCycle();
+                Registers[command[3]] += int.Parse(split[1]);
+            }
+
+            CheckCyclesAndAddIfNeeded();
+        }
+
+        private void IncrementCycle()
+        {
+            if ((Cycle - 1) % 40 == 0)
+            {
+                Console.WriteLine();
+            }
+
+            var pixelPos = Cycle - (Math.Floor(Cycle / 40) * 40) - 1;
+            var distance = Registers['x'] - pixelPos;
+            var overlapping = distance <= 1 && distance >= -1;
+
+            Console.Write(overlapping ? '#' : '.');
+
+            Cycle ++;
+        }
+
+        private void CheckCyclesAndAddIfNeeded()
+        {
+            if (Cycle == 20 || (Cycle - 20) % 40 == 0)
+            {
+                SignalStrength += Cycle * Registers['x'];
+            }
+        }
+    }
+}
---
+++ Day10/Program.cs
@@ -0,0 +1,14 @@
+﻿using Day10;
+
+var sampleProgramFile = "program-sample.txt";
+var programFile = "program.txt";
+
+var part1SampleAnalyzer = new Analyzer();
+var part1SampleSignalStrength = await part1SampleAnalyzer.GetSignalStrenghtSum(sampleProgramFile);
+Console.WriteLine();
+Console.WriteLine($"Day 10 Part 1 (Sample): Expected Signal Strength is 13140 got {part1SampleSignalStrength}");
+
+var part1Analyzer = new Analyzer();
+var part1SignalStrength = await part1Analyzer.GetSignalStrenghtSum(programFile);
+Console.WriteLine();
+Console.WriteLine($"Day 10 Part 1: Signal Strength is {part1SignalStrength}");
